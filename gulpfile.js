d476d21318c8afb048596de2d0d8b4d765393d4c
Astorsoft <astorz@gmail.com>
2017-08-01 16:23:36 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const plugins = gulpLoadPlugins();
+
+gulp.task('html')
+  .src('*.mjml')
+  .pipe(plugins.mjml())
+  .pipe(gulp.dest('./html'));
+
+
+gulp.task('default', ['html'], function() {
+  gulp.watch(['*.mjml'], ['html']);
+});
